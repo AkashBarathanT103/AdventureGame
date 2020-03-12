585ef473e9a381c8878f83218c280b64a60ae827
Akash <tjepicdude@gmail.com>
2020-03-12 09:32:31 -0400

    Disables buttons after player wins the game.

--- app/src/main/java/com/barathanakash/adventuregame/MainActivity.java
+++ app/src/main/java/com/barathanakash/adventuregame/MainActivity.java
@@ -7,11 +7,13 @@ import android.view.View;
 
 import java.io.IOException;
 import java.io.InputStream;
+
 import android.content.SharedPreferences;
 
 
 import android.widget.Button;
 import android.widget.TextView;
+
 import com.google.gson.Gson;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,39 +30,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        yes=findViewById(R.id.yesButton);
-        no=findViewById(R.id.noButton);
-        playerScore=findViewById(R.id.Text2);
-        highScore=findViewById(R.id.Text3);
+        yes = findViewById(R.id.yesButton);
+        no = findViewById(R.id.noButton);
+        playerScore = findViewById(R.id.Text2);
+        highScore = findViewById(R.id.Text3);
         final SharedPreferences sharedPreferences = getSharedPreferences("Settings", MODE_PRIVATE);
-        hscore=sharedPreferences.getInt("idTV1", hscore);
+        hscore = sharedPreferences.getInt("idTV1", hscore);
         final SharedPreferences.Editor editor = sharedPreferences.edit();
 
 
         Gson gson = new Gson();
         final Story story = gson.fromJson(loadJSONFromAsset(), Story.class);
-        displayText=findViewById(R.id.Text);
+        displayText = findViewById(R.id.Text);
         final Questions[] q = story.getQuestions();
         yes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int y = q[x].getAnswer().getYes();
-                score = score+q[x].getScore();
-                if(score>hscore){
-                    hscore=score;
-                }
-                if(hscore>45){
+                score = score + q[x].getScore();
+                if (score >= 45) {
+                    yes.setEnabled(false);
+                    no.setEnabled(false);
                     editor.remove("idTV1");
-                    hscore=0;
+                    hscore = 0;
                     editor.apply();
+                    displayText.setText("You've completed the game!");
+
+
                 }
+                if (score > hscore) {
+                    hscore = score;
+                }
+
+
                 editor.putInt("idTV1", hscore);
                 editor.apply();
 
-                playerScore.setText("Player Score: "+score);
-                highScore.setText("High Score: "+sharedPreferences.getInt("idTV1",hscore));
-                String s = q[y-1].getText();
-                x=y-1;
+                playerScore.setText("Player Score: " + score);
+                highScore.setText("High Score: " + sharedPreferences.getInt("idTV1", hscore));
+                String s = q[y - 1].getText();
+                x = y - 1;
                 displayText.setText(s);
 
             }
@@ -69,20 +78,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 int a = q[x].getAnswer().getNo();
-                score = score+q[x].getScore();
-                if(score>hscore){
-                    hscore=score;
+                score = score + q[x].getScore();
+                if (score >= 45) {
+                    yes.setEnabled(false);
+                    no.setEnabled(false);
+                    editor.remove("idTV1");
+                    hscore = 0;
+                    editor.apply();
+
+
                 }
-                if(hscore>45){
-                    editor.clear();
-                    editor.commit();
+
+                if (score > hscore) {
+                    hscore = score;
                 }
+
+
                 editor.putInt("idTV1", hscore);
                 editor.apply();
-                playerScore.setText("Player Score: "+score);
-                highScore.setText("High Score: "+sharedPreferences.getInt("idTV1",hscore));
-                String s = q[a-1].getText();
-                x=a-1;
+                playerScore.setText("Player Score: " + score);
+                highScore.setText("High Score: " + sharedPreferences.getInt("idTV1", hscore));
+                String s = q[a - 1].getText();
+                x = a - 1;
                 displayText.setText(s);
             }
         });
